59ace651b41a142fcfb3dbdfaabd8f04e0daff84
Andreas Lind <andreaslindpetersen@gmail.com>
2019-05-02 23:29:01 +0200

    Update snapshots

--- test/index.js
+++ test/index.js
@@ -35,26 +35,29 @@ describe('offline-github-changelog', () => {
     expect(
       getOutput(),
       'to match snapshot',
-      '### v1.0.1\n' +
-        '#### Pull requests\n' +
-        '\n' +
-        '- [#2](/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '#### Commits to master\n' +
-        '\n' +
-        '- [Release 1.0.1](/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '- [Another commit to master for 1.0.1](/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '- [Commit to master for 1.0.1](/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '### v1.0.0\n' +
-        '#### Pull requests\n' +
-        '\n' +
-        '- [#1](/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '#### Commits to master\n' +
-        '\n' +
-        '- [Release 1.0.0](/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '- [Commit to master before first release](/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n'
+      `
+      ### v1.0.1
+      #### Pull requests
+
+      - [#2](/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+      #### Commits to master
+
+      - [Release 1.0.1](/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+      - [Another commit to master for 1.0.1](/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+      - [Commit to master for 1.0.1](/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+      ### v1.0.0
+      #### Pull requests
+
+      - [#1](/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+      #### Commits to master
+
+      - [Release 1.0.0](/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+      - [Commit to master before first release](/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+    `
     );
   });
 
@@ -64,36 +67,39 @@ describe('offline-github-changelog', () => {
       expect(
         getOutput(),
         'to match snapshot',
-        '### v1.2.3 (2019-04-16)\n' +
-        '\n' +
-        '#### Pull requests\n' +
-        '\n' +
-        '- [#3](/pull/3) An unreleased feature ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '#### Commits to master\n' +
-        '\n' +
-        '- [Unreleased commit on master](/commit/d3987d8212cb43ce39255877f75d45780c00b19a) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '### v1.0.1\n' +
-        '#### Pull requests\n' +
-        '\n' +
-        '- [#2](/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '#### Commits to master\n' +
-        '\n' +
-        '- [Release 1.0.1](/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '- [Another commit to master for 1.0.1](/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '- [Commit to master for 1.0.1](/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '### v1.0.0\n' +
-        '#### Pull requests\n' +
-        '\n' +
-        '- [#1](/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '\n' +
-        '#### Commits to master\n' +
-        '\n' +
-        '- [Release 1.0.0](/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n' +
-        '- [Commit to master before first release](/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))\n'
+        `
+        ### v1.2.3 (2019-05-02)
+
+        #### Pull requests
+
+        - [#3](/pull/3) An unreleased feature ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+        #### Commits to master
+
+        - [Unreleased commit on master](/commit/d3987d8212cb43ce39255877f75d45780c00b19a) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+        ### v1.0.1
+        #### Pull requests
+
+        - [#2](/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+        #### Commits to master
+
+        - [Release 1.0.1](/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Another commit to master for 1.0.1](/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Commit to master for 1.0.1](/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+        ### v1.0.0
+        #### Pull requests
+
+        - [#1](/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+        #### Commits to master
+
+        - [Release 1.0.0](/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Commit to master before first release](/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+
+      `
       );
     });
   });
