befb9412f5ca16fab97d3e8a3d6a631971cc85a7
Sune Simonsen <sune@we-knowhow.dk>
2020-11-06 18:19:12 +0100

    Use the transformation library
    See https://github.com/sunesimonsen/transformation

--- test/index.js
+++ test/index.js
@@ -33,40 +33,40 @@ describe('offline-github-changelog', () => {
       .join('\n');
   }
 
-  it('should generate a changelog', () => {
-    generateChangelog();
+  it('should generate a changelog', async () => {
+    await generateChangelog('origin');
     expect(
       getOutput(),
       'to equal snapshot',
       expect.unindent`
         ### v1.0.1
+
         #### Pull requests
 
-        - [#2](/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [#2](https://github.com/papandreou/offline-github-changelog-test1/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
         #### Commits to master
 
-        - [Release 1.0.1](/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-        - [Another commit to master for 1.0.1](/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-        - [Commit to master for 1.0.1](/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Release 1.0.1](https://github.com/papandreou/offline-github-changelog-test1/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Another commit to master for 1.0.1](https://github.com/papandreou/offline-github-changelog-test1/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Commit to master for 1.0.1](https://github.com/papandreou/offline-github-changelog-test1/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
         ### v1.0.0
+
         #### Pull requests
 
-        - [#1](/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [#1](https://github.com/papandreou/offline-github-changelog-test1/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
         #### Commits to master
 
-        - [Release 1.0.0](/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-        - [Commit to master before first release](/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-
-      `
+        - [Release 1.0.0](https://github.com/papandreou/offline-github-changelog-test1/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+        - [Commit to master before first release](https://github.com/papandreou/offline-github-changelog-test1/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))`
     );
   });
 
   describe('with the next switch', () => {
-    it('should attribute the latest commits on master to the given version', () => {
-      generateChangelog(undefined, '1.2.3');
+    it('should attribute the latest commits on master to the given version', async () => {
+      await generateChangelog('origin', '1.2.3');
       expect(
         getOutput(),
         'to equal snapshot',
@@ -75,36 +75,36 @@ describe('offline-github-changelog', () => {
 
           #### Pull requests
 
-          - [#123](/pull/123) The title of some PR ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-          - [#3](/pull/3) An unreleased feature ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [#123](https://github.com/papandreou/offline-github-changelog-test1/pull/123) The title of some PR ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [#3](https://github.com/papandreou/offline-github-changelog-test1/pull/3) An unreleased feature ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
           #### Commits to master
 
-          - [HTML in commit message: &lt;html dir="rtl"&gt;](/commit/af22733adbc2fc4977862348f68d152b754b4516) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-          - [Unreleased commit on master](/commit/d3987d8212cb43ce39255877f75d45780c00b19a) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [HTML in commit message: &lt;html dir="rtl"&gt;](https://github.com/papandreou/offline-github-changelog-test1/commit/af22733adbc2fc4977862348f68d152b754b4516) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [Unreleased commit on master](https://github.com/papandreou/offline-github-changelog-test1/commit/d3987d8212cb43ce39255877f75d45780c00b19a) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
           ### v1.0.1
+
           #### Pull requests
 
-          - [#2](/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [#2](https://github.com/papandreou/offline-github-changelog-test1/pull/2) Feature for one oh one ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
           #### Commits to master
 
-          - [Release 1.0.1](/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-          - [Another commit to master for 1.0.1](/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-          - [Commit to master for 1.0.1](/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [Release 1.0.1](https://github.com/papandreou/offline-github-changelog-test1/commit/825179ea6b03098ebba60e433be57dfdcef2a3bd) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [Another commit to master for 1.0.1](https://github.com/papandreou/offline-github-changelog-test1/commit/f9509628af0dc9753b4d7a69467e8f060fba85d0) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [Commit to master for 1.0.1](https://github.com/papandreou/offline-github-changelog-test1/commit/e110b7590329843a20a26ffbb7b971ec3bfd9fd4) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
           ### v1.0.0
+
           #### Pull requests
 
-          - [#1](/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [#1](https://github.com/papandreou/offline-github-changelog-test1/pull/1) Feature commit before first release ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
 
           #### Commits to master
 
-          - [Release 1.0.0](/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-          - [Commit to master before first release](/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
-
-        `
+          - [Release 1.0.0](https://github.com/papandreou/offline-github-changelog-test1/commit/e1fe60089ad08d31929707a1713d711e9a49b58c) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))
+          - [Commit to master before first release](https://github.com/papandreou/offline-github-changelog-test1/commit/36851b522fc40ada3bb85d52d77183db23285143) ([Andreas Lind](mailto:andreaslindpetersen@gmail.com))`
       );
     });
   });
